65f5309cba8ac1aee13ee38c842173220ebd2d24
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2019-05-20 08:38:24 +0000

    form outline

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import React from "react"
+import PropTypes from "prop-types"
 
 const Form = props => {
   const formSubmit = e => {
@@ -12,7 +13,7 @@ const Form = props => {
     <div>
       <form onSubmit={formSubmit}>
         <input
-          className="w-full h-10 border-2 border-gray-900 p-6 focus:border-gray-900"
+          className="w-full h-10 border-2 border-gray-900 p-6 focus:border-gray-900 focus:outline-none"
           type="text"
           placeholder="search for photos"
           name="photos"
@@ -20,6 +21,10 @@ const Form = props => {
       </form>
     </div>
   )
+}
+
+Form.propTypes = {
+  fetchImages: PropTypes.object,
 }
 
 export default Form
